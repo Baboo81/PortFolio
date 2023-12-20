558957fe5af0456af1340503d5d19e83676f57b8
Baboo81 <magicbaboo@gmail.com>
2023-12-20 16:40:18 +0100

    SVG

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -33,9 +33,9 @@ requestAnimationFrame(ballAnimate1);
 //Footer map:
 
 //Create map:
-const centroid = [50.709925, 4.352603];
+const centroid = [50.71036, 4.36889];
 const map = L.map('mapid').setView(centroid, 16.4);
 
 //Add tiles & marker:
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-L.marker([50.709925, 4.352603]).addTo(map);
+L.marker([50.71036, 4.36889]).addTo(map);
