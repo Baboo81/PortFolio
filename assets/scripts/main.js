696e04104d60a890030ce63e95618547c6efb581
Baboo81 <magicbaboo@gmail.com>
2023-12-20 11:38:24 +0100

    Footer

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -29,3 +29,13 @@ function ballAnimate1 () {
 };
 
 requestAnimationFrame(ballAnimate1);
+
+//Footer map:
+
+//Create map:
+const centroid = [50.709925, 4.352603];
+const map = L.map('mapid').setView(centroid, 16.4);
+
+//Add tiles & marker:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.marker([50.709925, 4.352603]).addTo(map);
